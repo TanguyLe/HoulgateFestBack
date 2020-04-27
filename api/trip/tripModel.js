9cdf3a4d724e0bafb85fef7d1ca494d5ba0fc517
Tanguy Le Floch <lefloch.ta@gmail.com>
2020-04-27 23:24:00 +0200

    Updating trip model types

--- api/trip/tripModel.js
+++ api/trip/tripModel.js
@@ -6,12 +6,12 @@ let Schema = mongoose.Schema;
 
 let TripSchema = new Schema(
     {
-        date: {type: Date, required: true},
-        location: {type: String, required: true},
-        seats: {type: Number, min: [1, 'No places'], required: true},
+        date: {type: mongoose.Schema.Types.Date, required: true},
+        location: {type: mongoose.Schema.Types.String, required: true},
+        seats: {type: mongoose.Schema.Types.Number, min: [1, 'No places'], required: true},
         driver: {type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true},
         passengers: [{type: mongoose.Schema.Types.ObjectId, ref: 'Users'}],
-        type: {type: String, enum : ['BACK', 'FORTH'], required: true}
+        type: {type: mongoose.Schema.Types.String, enum: ["BACK", "FORTH"], required: true}
     }
 );
 

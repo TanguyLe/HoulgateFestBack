66d836f989b409a040d8662648db5ca02880c174
Tanguy Le Floch <lefloch.ta@gmail.com>
2020-05-07 19:34:50 +0200

    Create trip

--- api/trip/tripController.js
+++ api/trip/tripController.js
@@ -6,4 +6,11 @@ exports.getTrips = (callback, errCallback = (() => {})) => {
     Trip.find({}).then(callback).catch(errCallback)
 };
 
+exports.createTrip = (tripData, callback, errCallback = (() => {})) => {
+        if (tripData.passengers.length > tripData.seats)
+            errCallback({message: "More passengers than seats.", code: 400});
+        else
+            (new Trip(tripData)).save().then(callback).catch(errCallback);
+};
+
 exports.serializeTrip = (trip) => `At ${trip.date} from ${trip.location}`;

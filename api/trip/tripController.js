12fbef9060727401969271d0410d9bb9570a0331
Tanguy Le Floch <lefloch.ta@gmail.com>
2020-05-07 19:45:02 +0200

    PUT update

--- api/trip/tripController.js
+++ api/trip/tripController.js
@@ -2,15 +2,30 @@ let mongoose = require("mongoose"),
     Trip = mongoose.model("Trips");
 
 
+const validateTrip = (tripData) => {
+    if (tripData.passengers.length > tripData.seats)
+        return {message: "More passengers than seats.", code: 400};
+};
+
 exports.getTrips = (callback, errCallback = (() => {})) => {
     Trip.find({}).then(callback).catch(errCallback)
 };
 
 exports.createTrip = (tripData, callback, errCallback = (() => {})) => {
-        if (tripData.passengers.length > tripData.seats)
-            errCallback({message: "More passengers than seats.", code: 400});
-        else
-            (new Trip(tripData)).save().then(callback).catch(errCallback);
+        const err = validateTrip(tripData);
+        if (err)
+            errCallback(err);
+
+        (new Trip(tripData)).save().then(callback).catch(errCallback);
 };
+
+exports.updateTrip = (tripId, tripData, callback, errCallback = (() => {})) => {
+    const err = validateTrip(tripData);
+    if (err)
+        errCallback(err);
+
+    Trip.findByIdAndUpdate(tripId, tripData, {new: true}).then(callback).catch(errCallback);
+};
+
 
 exports.serializeTrip = (trip) => `At ${trip.date} from ${trip.location}`;

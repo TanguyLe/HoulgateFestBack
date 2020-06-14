91db7722b6e6d427d889584e37d04051d8a77d22
tanguy <lefloch.ta@gmail.com>
2020-06-14 16:24:56 +0200

    make get multiple users private

--- api/user/userRoutesWithAuth.js
+++ api/user/userRoutesWithAuth.js
@@ -1,6 +1,9 @@
 module.exports = (app) => {
     let user = require("./userController");
 
+    app.route("/users")
+        .get(user.userList);
+
     app.route("/users/:userId")
         .get(user.readUser);
 
